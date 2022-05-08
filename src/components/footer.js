619d0d1f8cf9e06947482a417bac073fc07d9bdd
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2022-05-09 00:19:42 +0300

    added mobile and tab responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import './footer.css'
 export const Footer = () => {
     return (
         <div className='footer'>
-            <p> Made with 🖤 By Dagmawi Babi </p>
+            <p> Made with 🖤 By <a href="https://t.me/dagmawi_babi"> Dagmawi Babi </a> </p>
         </div>
 
     )
